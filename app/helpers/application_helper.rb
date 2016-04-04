160066fe62325e0b70dadd35c1ec383074aa1b54
Robyn Jia Hui Liu <robyn.jh.liu@gmail.com>
2016-04-04 12:49:50 +0800

    Markdown complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def markdown(text)
+		Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+			no_intra_emphasis: true,
+			fenced_code_blocks: true,
+			disable_indented_code_blocks: true,
+			quote: true,
+			underline: true,
+			highlight: true
+			).render(text).html_safe
+	end
 end
